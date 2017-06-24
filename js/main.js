cf1b21767639375480387870c7aaa551d2ba8498
17Blaze <17blazergamer@gmail.com>
2017-06-23 15:05:08 -1000

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,17 +3,20 @@ function init(){
 }
 
 function preload(){
-	
-};
+	game.load.image('background', 'images/background.png');
+	game.load.json('Level1', 'data/level01.json');
+}
 
 function create(){
-	
+	game.add.image(0, 0, 'background');
+	loadlevel(this.game.cache.getJSON('Level1'));
 }
 
 function update(){
 
 }
 
-
-//Create a game state
-var game = new Phaser.Game(960, 600, Phaser.AUTO, 'game', {init: init, preload: preload, create: create, update: update});
+function loadlevel(data) {
+	console.log(data);
+}
+ar game = new Phaser.Game(960, 600, Phaser.AUTO, 'game', {init: init, preload: preload, create: create, update: update});
