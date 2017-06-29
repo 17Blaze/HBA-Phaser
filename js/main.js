b2b318e2eb0b0ea1fc14317d0b17aaaa817c1820
17Blaze <17blazergamer@gmail.com>
2017-06-28 14:53:00 -1000

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 function init() {}
 
 function preload() {
-    game.load.image('hero', 'images/hero_stopped.png');
+    game.load.audio('sfx:coin', 'audio/coin.wav')
+    game.load.image('hero', 'images/goku(5).png');
     game.load.image('background', 'images/background.png');
     game.load.json('level:1', 'data/level01.json');
     game.load.image('ground', 'images/ground.png');
@@ -10,9 +11,13 @@ function preload() {
     game.load.image('grass:1x1', 'images/grass_1x1.png');
     game.load.image('grass:8x1', 'images/grass_8x1.png');
     game.load.image('grass:6x1', 'images/grass_6x1.png');
+    game.load.audio('sfx:jump', 'audio/jump.wav');
+    game.load.spritesheet('coin', 'images/coin_animated.png', 22, 22);
 }
 
 function create() {
+    sfx:coin = game.add.audio('sfxcoin');
+    sfxJump = game.add.audio('sfx:jump');
     leftKey = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
     rightKey = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
     upKey = game.input.keyboard.addKey(Phaser.Keyboard.UP);
@@ -23,12 +28,22 @@ function create() {
     loadLevel(this.game.cache.getJSON('level:1'));
 }
 
+function jump(){
+    var canJump = hero.body.touching.down;
+    if (canJump) {
+        hero.body.velocity.y = -600;
+        sfxJump.play();
+    }
+    return canJump;
+}
+
 function update() {
     handleCollisions();
     handleInput();
 }
 
 function loadLevel(data) {
+    coins = game.add.group();
     platforms = game.add.group();
     game.physics.arcade.gravity.y = 1200;
     console.log(data);
@@ -48,14 +63,20 @@ function spawnPlatform(platform) {
 
 function spawnCharacters(data) {
     hero = game.add.sprite(data.hero.x, data.hero.y, 'hero');
-    hero.anchor.set(0.5, 0.5);
+    hero.anchor.set(0.5, 0.7);
     game.physics.enable(hero);
     hero.body.collideWorldBounds = true;
 }
-function jump() {
-    
-    hero.body.velocity.y = -600;
-}
+
+function spawnCoin(coin) {
+    data.coins.forEach(spawnCoin, this);
+    var sprite = coins.create(coin.x, coin.y, 'coin');
+    sprite.anchor.set(0.5, 0.5);
+    sprite.animations.add('rotate', [0, 1, 2, 1], 6, true);
+    sprite.animations.play('rotate');
+    this.game.physics.enable(sprite);
+    sprite.body.allowGravity = false;
+};
 
 function move(direction){
     hero.body.velocity.x = direction * 200;
@@ -82,6 +103,10 @@ function handleInput() {
 }
 function handleCollisions(){
    game.physics.arcade.collide(hero, platforms);
+   game.physics.arcade.overlap(hero, coins, onHeroVsCoin, null, this);
+};
+function onHeroVsCoin(hero, coin){
+    coin.kill();
 };
 var game = new Phaser.Game(960, 600, Phaser.AUTO, 'game', {init: init,preload: preload,create: create,update: update
 });
